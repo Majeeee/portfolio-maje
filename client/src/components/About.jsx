a26429e2e5b5379ca3aeced19d6b9e4d2a3baab8
Majeeee <Majid.Tajik@stud.sti.se>
2025-10-02 22:45:31 +0200

    feat(client): add About, Skills and Projects components

---
+++ client/src/components/About.jsx
@@ -0,0 +1,10 @@
+export default function About(){
+return (
+<section>
+<h2>Om mig</h2>
+<p>Kort beskrivning... (Jag har erfarenhet inom transaktionssäkra system och har bland annat jobbat med bank, HR, orginazation, distribution,lagar och orginazation,börshandelsystem. Jag är en social och kommunikativ person som har lätt att lära mig nya saker. En analytiker som är bra på att lösa problem i team som på egen hand. Jag är noggrann med en driv att ge mig på nya områden för att lära mig nya saker. 
+Jag är van vid att jobba inom både mindre och större organisationer och har erfarenhet att arbeta med kravanalys, systemdesign och systemutveckling inom större projekt.
+)</p>
+</section>
+)
+}
